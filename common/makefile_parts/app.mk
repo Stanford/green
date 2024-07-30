678ae7e307a90ce7e83d033768be04c5090f3d07
Adam H. Lewenberg <adamhl@stanford.edu>
2024-07-30 05:09:12 -0700

    add mypy target

--- common/makefile_parts/app.mk
+++ common/makefile_parts/app.mk
@@ -9,3 +9,10 @@ tests: ## Run unit tests
 	@source ${BUILD_DIR}/venv/bin/activate \
 	&& cd .. \
 	&& ./run-tests.sh
+
+.PHONY: mypy
+mypy:  ## Run Python static type-checker mypy
+	@source ${BUILD_DIR}/venv/bin/activate \
+	&& cd .. \
+	&& mypy --strict-equality --allow-subclassing-any --allow-any-generics --strict stanford/
+
